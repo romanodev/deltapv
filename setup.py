902951770fa2fa6e9515c471615ed61f26ea6e7d
Giuseppe Romano <gromano@pop-os.localdomain>
2019-05-20 18:04:45 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(name='jax-pv',
+      version='0.0.1',
+      description='Solar cell simulator with automatic differentiation',
+      author='Ekin Dogus Cubuk, Sam Schoenholz, Eric Richard Fadel and Giuseppe Romano ',
+      classifiers=['Programming Language :: Python :: 3.6'],
+      long_description=open('README.rst').read(),
+      install_requires=[],
+      license='MIT',\
+      packages = ['jax-pv'])
