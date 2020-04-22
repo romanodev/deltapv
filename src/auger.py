68379ae5638901272d6813f496fb02050be9e6e6
Eric Fadel <eric.fadel@gmail.com>
2020-04-22 15:58:43 -0400

    Debug.

--- src/auger.py
+++ src/auger.py
@@ -34,7 +34,7 @@ def auger( phi_n , phi_p , phi , Chi , Eg , Nc , Nv , Cn , Cp ):
     _ni = ni( Eg , Nc , Nv )
     _n = n( phi_n , phi , Chi , Nc )
     _p = p( phi_p , phi , Chi , Eg , Nv )
-    return ( Cn * n + Cp * p ) * ( _n * _p - _ni**2 )
+    return ( Cn * _n + Cp * _p ) * ( _n * _p - _ni**2 )
 
 
 
@@ -82,8 +82,8 @@ def auger_deriv( phi_n , phi_p , phi , Chi , Eg , Nc , Nv , Cn , Cp ):
     _n = n( phi_n , phi , Chi , Nc )
     _p = p( phi_p , phi , Chi , Eg , Nv )
 
-    DR_phin = ( Cn * n ) * ( _n * _p - _ni**2 ) + ( Cn * n + Cp * p ) * ( n * p )
-    DR_phip = ( - Cp * p ) * ( _n * _p - _ni**2 ) + ( Cn * n + Cp * p ) * ( - n * p )
-    DR_phi = ( Cn * n - Cp * p ) * ( _n * _p - _ni**2 )
+    DR_phin = ( Cn * _n ) * ( _n * _p - _ni**2 ) + ( Cn * _n + Cp * _p ) * ( _n * _p )
+    DR_phip = ( - Cp * _p ) * ( _n * _p - _ni**2 ) + ( Cn * _n + Cp * _p ) * ( - _n * _p )
+    DR_phi = ( Cn * _n - Cp * _p ) * ( _n * _p - _ni**2 )
 
     return DR_phin , DR_phip , DR_phi
