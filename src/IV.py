7b0b58f55293e0fefef4d776b7b543ff71c14638
Eric Fadel <eric.fadel@gmail.com>
2020-04-23 01:40:04 -0400

    Debug.

--- src/IV.py
+++ src/IV.py
@@ -272,6 +272,7 @@ def grad_IV( dgrid , Vincrement , eps , Chi , Eg , Nc , Nv , Ndop , mn , mp , Et
         current.append( tot_current )
 
         jac_phis['eps'] = gradsol[5] + np.dot( gradsol[4] , jac_phis['eps'] )
+        u = np.dot( gradsol[0] , dneq0_dChi.transpose() )
         jac_phis['Chi'] = gradsol[6] + np.dot( gradsol[0] , dneq0_dChi.transpose() ) + np.dot( gradsol[1] , dneqL_dChi.transpose() ) + np.dot( gradsol[2] , dpeq0_dChi.transpose() ) + np.dot( gradsol[3] , dpeqL_dChi.transpose() ) + np.dot( gradsol[4] , jac_phis['Chi'] )
         jac_phis['Eg'] = gradsol[7] + np.dot( gradsol[0] , dneq0_dEg.transpose() ) + np.dot( gradsol[1] , dneqL_dEg.transpose() ) + np.dot( gradsol[2] , dpeq0_dEg.transpose() ) + np.dot( gradsol[3] , dpeqL_dEg.transpose() )+ np.dot( gradsol[4] , jac_phis['Eg'] )
         jac_phis['Nc'] = gradsol[8] + np.dot( gradsol[0] , dneq0_dNc.transpose() ) + np.dot( gradsol[1] , dneqL_dNc.transpose() ) + np.dot( gradsol[2] , dpeq0_dNc.transpose() ) + np.dot( gradsol[3] , dpeqL_dNc.transpose() )+ np.dot( gradsol[4] , jac_phis['Nc'] )
