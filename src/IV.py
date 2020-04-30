bc349e87dbac51061b164ab5753bc8af5de5c694
Eric Fadel <eric.fadel@gmail.com>
2020-04-30 05:42:16 -0400

    Debug.

--- src/IV.py
+++ src/IV.py
@@ -99,7 +99,7 @@ def calc_IV( dgrid , Vincrement , eps , Chi , Eg , Nc , Nv , Ndop , mn , mp , Et
 def function_test( dgrid , eps , Chi , Eg , Nc , Nv , Ndop ):
     phi_ini = eq_init_phi( Chi , Eg , Nc , Nv , Ndop )
     phieq = solve_eq( dgrid , phi_ini , eps , Chi , Eg , Nc , Nv , Ndop )
-    return p( np.zeros( dgrid.size + 1 ) , phieq , Chi , Eg , Nv )[0]
+    return p( np.zeros( dgrid.size + 1 ) , phieq , Chi , Eg , Nv )[-1]
 
 
 def grad_IV( dgrid , Vincrement , eps , Chi , Eg , Nc , Nv , Ndop , mn , mp , Et , tn , tp , Br , Cn , Cp , Snl , Spl , Snr , Spr , G_used ):
@@ -237,12 +237,12 @@ def grad_IV( dgrid , Vincrement , eps , Chi , Eg , Nc , Nv , Ndop , mn , mp , Et
     grad_test = jacfwd( function_test , ( 1 , 2 , 3 , 4 , 5 , 6 ) )
     gradtest = grad_test( dgrid , eps , Chi , Eg , Nc , Nv , Ndop )
 
-    print( np.allclose( gradtest[0] , dpeq0_deps ) )
-    print( np.allclose( gradtest[1] , dpeq0_dChi ) )
-    print( np.allclose( gradtest[2] , dpeq0_dEg ) )
-    print( np.allclose( gradtest[3] , dpeq0_dNc ) )
-    print( np.allclose( gradtest[4] , dpeq0_dNv ) )
-    print( np.allclose( gradtest[5] , dpeq0_dNdop ) )
+    print( np.allclose( gradtest[0] , dpeqL_deps ) )
+    print( np.allclose( gradtest[1] , dpeqL_dChi ) )
+    print( np.allclose( gradtest[2] , dpeqL_dEg ) )
+    print( np.allclose( gradtest[3] , dpeqL_dNc ) )
+    print( np.allclose( gradtest[4] , dpeqL_dNv ) )
+    print( np.allclose( gradtest[5] , dpeqL_dNdop ) )
 
     quit()
 
