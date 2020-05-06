c8d3905e916ce153114360b068c36ac0848f6292
Eric Fadel <eric.fadel@gmail.com>
2020-05-06 19:44:51 -0400

    Debug.

--- src/current.py
+++ src/current.py
@@ -37,6 +37,8 @@ def Jn( dgrid , phi_n , phi , Chi , Nc , mn ):
 
     psi_n = Chi + np.log( Nc ) + phi
 
+    print( phi_n )
+    print( psi_n )
 
     Dpsin = psi_n[:-1] - psi_n[1:]
     thr = 1e-5
