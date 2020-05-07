b8bc26a6b065c0c68356450c3d3f0cae461335b9
Eric Fadel <eric.fadel@gmail.com>
2020-05-07 02:54:42 -0400

    Debug.

--- src/current.py
+++ src/current.py
@@ -301,7 +301,7 @@ def total_current( dgrid , phi_n , phi_p , phi , Chi , Eg , Nc , Nv , mn , mp ):
     dfmn_dphin0 = - np.exp( phi_n[0] )
     dfmn_dphin1 = np.exp( phi_n[1] )
     numerator2 = ( 1 - around_zero_n ) * ( - Dpsin + np.exp( Dpsin ) - 1 ) + around_zero_n * ( - 3 + psin0 + psin1 + 2*psin0*psin1 - psin0**2 - psin1**2 )
-    denominator2 = ( 1 - around_zero_n ) * ( np.exp( Dpsin ) - 1 )**2 + around_zero_n * ( 1 + 0.5*psin0 - 0.5*psin1] - 1/3.0*psin0*psin1 + 1/6.0*psin0]**2 + 1/6.0*psin1**2 )**2
+    denominator2 = ( 1 - around_zero_n ) * ( np.exp( Dpsin ) - 1 )**2 + around_zero_n * ( 1 + 0.5*psin0 - 0.5*psin1 - 1/3.0*psin0*psin1 + 1/6.0*psin0]**2 + 1/6.0*psin1**2 )**2
     numerator3 = ( 1 - around_zero_n ) * ( - np.exp( Dpsin ) + 1 + Dpsin * np.exp( Dpsin ) ) + around_zero_n * ( - 3 - 2*psin0 + 2*psin1 )
     denominator3 = ( 1 - around_zero_n ) * ( np.exp( Dpsin ) - 1 )**2 + around_zero_n * ( 1 + 0.5*psin0 - 0.5*psin1 - 1/3.0*psin0*psin1 + 1/6.0*psin0**2 + 1/6.0*psin1**2 )**2
     Dpsin_Dexppsin_dpsin0 = np.exp( psin0 ) * _numerator2 / _denominator2
