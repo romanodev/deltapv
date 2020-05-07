27f6c45af224f8edd3a98aab25c392cdc3d5727b
Eric Fadel <eric.fadel@gmail.com>
2020-05-07 02:23:20 -0400

    Debug.

--- src/boundary_conditions.py
+++ src/boundary_conditions.py
@@ -90,7 +90,9 @@ def contact_phin_deriv( dgrid , phi_n , phi , Chi , Nc , mn , Snl , Snr ):
     _n = n( phi_n , phi , Chi , Nc )
     dJn_phin_maindiag , dJn_phin_upperdiag , dJn_phi_maindiag , dJn_phi_upperdiag = Jn_deriv( dgrid , phi_n , phi , Chi , Nc , mn )
 
-    print( _n )
+    print( _n[0] )
+    print( dJn_phin_maindiag[0] )
+    print( Snl )
     print( dJn_phin_maindiag[0] - Snl * _n[0] )
     quit()
 
