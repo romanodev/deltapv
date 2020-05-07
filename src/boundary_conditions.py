a12e6524a3c6744e6ca9a8b94441757503ce9c15
Eric Fadel <eric.fadel@gmail.com>
2020-05-07 01:15:37 -0400

    Debug.

--- src/boundary_conditions.py
+++ src/boundary_conditions.py
@@ -89,11 +89,17 @@ def contact_phin_deriv( dgrid , phi_n , phi , Chi , Nc , mn , Snl , Snr ):
     """
     _n = n( phi_n , phi , Chi , Nc )
     dJn_phin_maindiag , dJn_phin_upperdiag , dJn_phi_maindiag , dJn_phi_upperdiag = Jn_deriv( dgrid , phi_n , phi , Chi , Nc , mn )
-
     return dJn_phin_maindiag[0] - Snl * _n[0] , dJn_phin_upperdiag[0] , \
+    dJn_phin_maindiag[-1] + Snr * _n[-1] , dJn_phin_upperdiag[-1] , \
     dJn_phi_maindiag[0] - Snl * _n[0] , dJn_phi_upperdiag[0] , \
-    dJn_phin_maindiag[-1] , dJn_phin_upperdiag[-1] + Snr * _n[-1] , \
-    dJn_phi_maindiag[-1] , dJn_phi_upperdiag[-1] + Snr * _n[-1]
+    dJn_phi_maindiag[-1] + Snr * _n[-1] , dJn_phi_upperdiag[-1]
+
+
+
+#    return dJn_phin_maindiag[0] - Snl * _n[0] , dJn_phin_upperdiag[0] , \
+#    dJn_phi_maindiag[0] - Snl * _n[0] , dJn_phi_upperdiag[0] , \
+#    dJn_phin_maindiag[-1] , dJn_phin_upperdiag[-1] + Snr * _n[-1] , \
+#    dJn_phi_maindiag[-1] , dJn_phi_upperdiag[-1] + Snr * _n[-1]
 
 
 
@@ -191,8 +197,14 @@ def contact_phip_deriv( dgrid , phi_p , phi , Chi , Eg , Nv , mp , Spl , Spr ):
     """
     _p = p( phi_p , phi , Chi , Eg , Nv )
     dJp_phip_maindiag , dJp_phip_upperdiag , dJp_phi_maindiag , dJp_phi_upperdiag = Jp_deriv( dgrid , phi_p , phi , Chi , Eg , Nv , mp )
+    return dJp_phip_maindiag[0] + Spl * ( - _p[0] ) , dJp_phip_upperdiag[0] , \
+    dJp_phip_maindiag[-1] , dJp_phip_upperdiag[-1]  - Spr * ( - _p[-1] ) , \
+    dJp_phi_maindiag[0] + Spl * ( - _p[0] ) , dJp_phi_upperdiag[0] , \
+    dJp_phi_maindiag[-1] , dJp_phi_upperdiag[-1]  - Spr * ( - _p[-1] )
 
-    return dJp_phip_maindiag[0] - Spl * _p[0] , dJp_phip_upperdiag[0] , \
-    dJp_phi_maindiag[0] - Spl * _p[0] , dJp_phi_upperdiag[0] , \
-    dJp_phip_maindiag[-1] , dJp_phip_upperdiag[-1] + Spr * _p[-1] , \
-    dJp_phi_maindiag[-1] , dJp_phi_upperdiag[-1] + Spr * _p[-1]
+
+
+#    return dJp_phip_maindiag[0] - Spl * _p[0] , dJp_phip_upperdiag[0] , \
+#    dJp_phi_maindiag[0] - Spl * _p[0] , dJp_phi_upperdiag[0] , \
+#    dJp_phip_maindiag[-1] , dJp_phip_upperdiag[-1] + Spr * _p[-1] , \
+#    dJp_phi_maindiag[-1] , dJp_phi_upperdiag[-1] + Spr * _p[-1]
