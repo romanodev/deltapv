37fb1aa1db8d19b31f33fe70989650fdb7dc6db6
Eric Fadel <eric.fadel@gmail.com>
2020-05-06 23:17:59 -0400

    Debug.

--- src/F.py
+++ src/F.py
@@ -80,6 +80,9 @@ def F( dgrid , neq_0 , neq_L , peq_0 , peq_L , phi_n , phi_p , phi , eps , Chi ,
     ctct_0_phin , ctct_L_phin = contact_phin( dgrid , neq_0 , neq_L , phi_n , phi , Chi , Nc , mn , Snl , Snr )
     ctct_0_phip , ctct_L_phip = contact_phip( dgrid , peq_0 , peq_L , phi_p , phi , Chi , Eg , Nv , mp , Spl , Spr )
 
+    print(pois)
+    quit()
+
     result = [ ctct_0_phin , ctct_0_phip , 0.0 ]
     for i in range( len(_pois) ):
         result.append( _ddn[i] )
