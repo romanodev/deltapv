0b542222fa5935e1953796daef7425703316174b
Eric Fadel <eric.fadel@gmail.com>
2020-05-07 00:44:41 -0400

    Debug.

--- src/solve.py
+++ src/solve.py
@@ -99,14 +99,14 @@ def step( dgrid , neq0 , neqL , peq0 , peqL , phis , eps , Chi , Eg , Nc , Nv , 
 
     _F = F( dgrid , neq0 , neqL , peq0 , peqL , phis[0:N] , phis[N:2*N] , phis[2*N:] , eps , Chi , Eg , Nc , Nv , Ndop , mn , mp , Et , tn , tp , Br , Cn , Cp , Snl , Spl , Snr , Spr , G )
 
-    print( _F )
+#    print( _F )
 
-    new_phis = phis + np.concatenate( ( np.zeros(N) , np.array([1000]) , np.zeros(2*N-1)   ) , axis = 0 )
+#    new_phis = phis + np.concatenate( ( np.zeros(N) , np.array([1000]) , np.zeros(2*N-1)   ) , axis = 0 )
 
-    _F = F( dgrid , neq0 , neqL , peq0 , peqL , new_phis[0:N] , new_phis[N:2*N] , new_phis[2*N:] , eps , Chi , Eg , Nc , Nv , Ndop , mn , mp , Et , tn , tp , Br , Cn , Cp , Snl , Spl , Snr , Spr , G )
+#    _F = F( dgrid , neq0 , neqL , peq0 , peqL , new_phis[0:N] , new_phis[N:2*N] , new_phis[2*N:] , eps , Chi , Eg , Nc , Nv , Ndop , mn , mp , Et , tn , tp , Br , Cn , Cp , Snl , Spl , Snr , Spr , G )
 
-    print( _F )
-    quit()
+#    print( _F )
+#    quit()
 
     gradF = F_deriv( dgrid , neq0 , neqL , peq0 , peqL , phis[0:N] , phis[N:2*N] , phis[2*N:] , eps , Chi , Eg , Nc , Nv , Ndop , mn , mp , Et , tn , tp , Br , Cn , Cp , Snl , Spl , Snr , Spr , G )
 
@@ -118,7 +118,7 @@ def step( dgrid , neq0 , neqL , peq0 , peqL , phis , eps , Chi , Eg , Nc , Nv , 
 
     move = np.linalg.solve( gradF , - _F )
 
-#    print( move )
+    print( move )
     quit()
     error = np.linalg.norm( move )
     damp_move = damp( move )
