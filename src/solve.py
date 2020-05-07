5a1e940e1775fbe6e0a66d0b7b13887b67307ec9
Eric Fadel <eric.fadel@gmail.com>
2020-05-07 02:10:32 -0400

    Debug.

--- src/solve.py
+++ src/solve.py
@@ -101,11 +101,12 @@ def step( dgrid , neq0 , neqL , peq0 , peqL , phis , eps , Chi , Eg , Nc , Nv , 
 
     gradF = F_deriv( dgrid , neq0 , neqL , peq0 , peqL , phis[0:N] , phis[N:2*N] , phis[2*N:] , eps , Chi , Eg , Nc , Nv , Ndop , mn , mp , Et , tn , tp , Br , Cn , Cp , Snl , Spl , Snr , Spr , G )
 
+    print( phis )
     print( _F )
-#    for i in range(3*N):
-#        for j in range(3*N):
-#            if (gradF[i,j]!=0):
-#                print(i,j,gradF[i,j])
+    for i in range(3*N):
+        for j in range(3*N):
+            if (gradF[i,j]!=0):
+                print(i,j,gradF[i,j])
 
     move = np.linalg.solve( gradF , - _F )
 
