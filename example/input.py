0864d1340f5b72925685209a410a21cbadcf09a4
Eric Fadel <eric.fadel@gmail.com>
2020-05-24 11:11:54 +0200

    Incident light not needed when providing G.

--- example/input.py
+++ example/input.py
@@ -69,8 +69,6 @@ pv_obj.contacts(10e7, 10e7, 10e7, 10e7)
 
 pv_obj.optical_G(type='user', G=1e18 * np.ones(NUM_POINTS, dtype=np.float32))
 
-pv_obj.incident_light()
-
 eq_result = pv_obj.solve(equilibrium=False,V=1)
 #pv_obj.plot_concentration_profile(eq_result)
 #pv_obj.plot_band_diagram(eq_result)
