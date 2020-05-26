ad74ec1c6369ace21d40dd687891af5800719d67
Sean Mann <63311354+sean-mann@users.noreply.github.com>
2020-05-26 14:22:58 +0800

    rcond=1e-7

--- jaxpv/solve.py
+++ jaxpv/solve.py
@@ -103,7 +103,7 @@ def step( dgrid , neq0 , neqL , peq0 , peqL , phis , eps , Chi , Eg , Nc , Nv , 
 
     move_solve = np.linalg.solve( gradF , - _F )
     
-    move,_,_,_ = np.linalg.lstsq(gradF, -_F, rcond=1e-10)
+    move,_,_,_ = np.linalg.lstsq(gradF, -_F, rcond=1e-7)
     
     error = np.linalg.norm(move)
     damp_move = damp( move )
