b2e813e9ebc9f1d0e15e0a57f5177be86cc70126
Eric Fadel <eric.fadel@gmail.com>
2020-05-29 18:54:29 +0200

    Debug.

--- jaxpv/solve.py
+++ jaxpv/solve.py
@@ -101,8 +101,6 @@ def step( dgrid , neq0 , neqL , peq0 , peqL , phis , eps , Chi , Eg , Nc , Nv , 
     _F = F( dgrid , neq0 , neqL , peq0 , peqL , phis[0:N] , phis[N:2*N] , phis[2*N:] , eps , Chi , Eg , Nc , Nv , Ndop , mn , mp , Et , tn , tp , Br , Cn , Cp , Snl , Spl , Snr , Spr , G )
     gradF = F_deriv( dgrid , neq0 , neqL , peq0 , peqL , phis[0:N] , phis[N:2*N] , phis[2*N:] , eps , Chi , Eg , Nc , Nv , Ndop , mn , mp , Et , tn , tp , Br , Cn , Cp , Snl , Spl , Snr , Spr , G )
 
-    gradF = ( np.abs( gradF ) > 1e-30 ) * gradF
-
     try:
         move = np.linalg.solve( gradF , - _F )
     except np.linalg.LinAlgError as err:
