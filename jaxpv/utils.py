8818fe1bf46425512de9bc57d6c20c6aa12a7650
Sean Mann <seanmann@node0040.inband>
2020-11-05 17:09:44 -0500

    --

--- jaxpv/utils.py
+++ jaxpv/utils.py
@@ -1,13 +1,13 @@
 import os
 
 if os.environ['JAX'] == 'YES':
-    print("JAX")
+    print("JAX available. Using JAX.")
     from jax.config import config
     config.update("jax_enable_x64", True)
     import jax.numpy as np
     from jax import grad , jit, jacfwd, ops
-    ops
 else:
+    print("JAX unavailable.")
     import numpy as np
 
 
