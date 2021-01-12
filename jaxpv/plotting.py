00c6b6ce1fc87426140389480bb4452c53d8db95
seanmann <sean.mann.hk@gmail.com>
2021-01-11 22:06:13 -0600

    plotting

--- jaxpv/plotting.py
+++ jaxpv/plotting.py
@@ -8,14 +8,23 @@ PVDesign = objects.PVDesign
 Potentials = objects.Potentials
 Array = util.Array
 
+COLORS = ["darkorange", "yellow", "limegreen", "cyan", "indigo"]
+
 
 def plot_bars(design: PVDesign) -> None:
 
+    _, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
+    ax1.set_zorder(1)
+    ax1.patch.set_visible(False)
+
     Ec = -scales.energy * design.Chi
     Ev = -scales.energy * (design.Chi + design.Eg)
+    dim_grid = scales.length * design.grid
 
-    _, idx = np.unique(Ec, return_index=True)
-    idx = np.sort(idx)
+    idx = np.concatenate(
+        [np.array([0]),
+         np.argwhere(Ec[:-1] != Ec[1:]).flatten() + 1])
 
     uc = Ec[idx]
     uv = Ev[idx]
@@ -29,22 +38,44 @@ def plot_bars(design: PVDesign) -> None:
         rect = Rectangle((x, y),
                          w,
                          h,
-                         facecolor="cornflowerblue",
-                         edgecolor="white",
-                         linewidth=10)
-        plt.text(x + w / 2,
+                         color=COLORS[i % len(COLORS)],
+                         linewidth=0, alpha=.2)
+        ax1.add_patch(rect)
+        ax1.text(x + w / 2,
                  y + h + .1,
                  round(y + h, 2),
                  ha="center",
-                 va="center")
-        plt.text(x + w / 2, y - .1, round(y, 2), ha="center", va="center")
-        plt.gca().add_patch(rect)
+                 va="bottom")
+        ax1.text(x + w / 2, y - .1, round(y, 2), ha="center", va="top")
 
-    plt.xlim(0, scales.length * design.grid[-1])
-    plt.ylim(np.min(uv) * 1.2, 0)
+    ax1.set_xlim(0, scales.length * design.grid[-1])
+    ax1.set_ylim(np.min(uv) * 1.2, 0)
+    ax1.set_xlabel("position / cm")
+    ax1.set_ylabel("energy / eV")
 
-    plt.xlabel("position / cm")
-    plt.ylabel("energy / eV")
+    ax2.set_yscale("log")
+    dim_Ndop = scales.density * design.Ndop
+
+    ax2.plot(dim_grid,
+             dim_Ndop,
+             label="donor",
+             color="lightcoral")
+
+    ax2.plot(dim_grid,
+             -dim_Ndop,
+             label="acceptor",
+             color="cornflowerblue")
+
+    posline = np.argwhere(dim_Ndop[:-1] != dim_Ndop[1:]).flatten()
+
+    for x in posline:
+        ax2.axvline(dim_grid[x], color="white", linewidth=2)
+        ax2.axvline(dim_grid[x + 1], color="white", linewidth=2)
+        ax2.axvline(dim_grid[x], color="lightgray", linewidth=1, linestyle="dashed")
+
+    ax2.margins(y=.5)
+    ax2.legend()
+    ax2.set_ylabel("doping / cm^(-3)")
 
     plt.show()
 
@@ -55,14 +86,31 @@ def plot_band_diagram(design: PVDesign, pot: Potentials, eq=False) -> None:
     Ev = -scales.energy * (design.Chi + design.Eg + pot.phi)
     x = scales.length * design.grid
 
-    plt.plot(x, Ec, color="red", label="conduction band", linestyle="dashed")
-    plt.plot(x, Ev, color="blue", label="valence band", linestyle="dashed")
+    plt.plot(x,
+             Ec,
+             color="lightcoral",
+             label="conduction band",
+             linestyle="dashed")
+    plt.plot(x,
+             Ev,
+             color="cornflowerblue",
+             label="valence band",
+             linestyle="dashed")
 
     if not eq:
-        plt.plot(x, scales.energy * pot.phi_n, color="red", label="e- quasi-Fermi energy")
-        plt.plot(x, scales.energy * pot.phi_p, color="blue", label="hole quasi-Fermi energy")
+        plt.plot(x,
+                 scales.energy * pot.phi_n,
+                 color="lightcoral",
+                 label="e- quasi-Fermi energy")
+        plt.plot(x,
+                 scales.energy * pot.phi_p,
+                 color="cornflowerblue",
+                 label="hole quasi-Fermi energy")
     else:
-        plt.plot(x, scales.energy * pot.phi_p, color="grey", label="Fermi level")
+        plt.plot(x,
+                 scales.energy * pot.phi_p,
+                 color="lightgray",
+                 label="Fermi level")
 
     plt.xlabel("position / cm")
     plt.ylabel("energy / eV")
