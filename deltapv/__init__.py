4805440535f4669fd7d35eb6b2bc066c8e6de4f2
seanmann <sean.mann.hk@gmail.com>
2021-04-10 01:49:25 -0400

    tutorial

--- deltapv/__init__.py
+++ deltapv/__init__.py
@@ -11,7 +11,7 @@ logging.basicConfig(format="")
 logger = logging.getLogger("deltapv")
 logger.setLevel("INFO")
 
-from deltapv import simulator, materials, plotting, objects, spline, util
+from deltapv import simulator, materials, plotting, objects, spline, physics, util
 from deltapv.simulator import make_design, incident_light, equilibrium, simulate, empty_design, add_material, doping, contacts
 from deltapv.materials import create_material, load_material
 from deltapv.plotting import plot_band_diagram, plot_bars, plot_charge, plot_iv_curve
