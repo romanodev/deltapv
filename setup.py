c7a86d72932cf7916aff0599f81ea24fb7eba164
Giuseppe Romano <romanog@mit.edu>
2025-09-28 11:03:22 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="deltapv",
-    version="0.0.3",
+    version="0.0.5",
     description="Solar cell simulator with automatic differentiation",
     author=
     "Sean Mann, Eric Richard Fadel, Ekin Dogus Cubuk, Sam Schoenholz, and Giuseppe Romano",
     classifiers=["Programming Language :: Python :: 3.7"],
-    long_description=open("README.rst").read(),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     install_requires=[
         "numpy", "jax", "jaxlib","matplotlib", "pandas", "equinox","pyyaml", "scipy"
     ],
